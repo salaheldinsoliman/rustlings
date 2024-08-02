56c2704490b9ec543ed80d667006df57e88fe61e
salaheldinsoliman <salaheldin_sameh@aucegypt.edu>
2024-08-02 02:04:47 +0200

    Solve stuff
    Signed-off-by: salaheldinsoliman <salaheldin_sameh@aucegypt.edu>

---
+++ exercises/18_iterators/iterators1.rs
@@ -0,0 +1,22 @@
+// When performing operations on elements within a collection, iterators are
+// essential. This module helps you get familiar with the structure of using an
+// iterator and how to go through elements within an iterable collection.
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn iterators() {
+        let my_fav_fruits = ["banana", "custard apple", "avocado", "peach", "raspberry"];
+
+       
+       let iter = my_fav_fruits.iter();
+
+       while let Some(x) = iter.next() {
+        f(x);
+    }
+    }
+}
